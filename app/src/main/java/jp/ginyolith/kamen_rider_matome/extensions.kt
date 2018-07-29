50d5f3baa62ef3f897e47afe56eaed0ca655a0cd
Fumiya Akagi <fumiya.akagi@iridge.jp>
2018-07-29 21:07:28 +0900

    refactor : recyclerViewの区切り線設定を拡張関数化

--- app/src/main/java/jp/ginyolith/kamen_rider_matome/extensions.kt
+++ app/src/main/java/jp/ginyolith/kamen_rider_matome/extensions.kt
@@ -3,6 +3,9 @@ package jp.ginyolith.kamen_rider_matome
 import android.app.Activity
 import android.app.AlertDialog
 import android.content.DialogInterface
+import android.support.v7.widget.DividerItemDecoration
+import android.support.v7.widget.LinearLayoutManager
+import android.support.v7.widget.RecyclerView
 import android.widget.Toast
 
 fun Activity.toast(msg : String) {
@@ -22,4 +25,14 @@ fun Activity.dialog(title : String, msg : String) {
 
 fun Activity.runOnUiThread(exec : () -> Unit) {
     this.runOnUiThread(java.lang.Runnable(exec))
+}
+
+fun RecyclerView.setBorder(enabled : Boolean) {
+    val decoration = DividerItemDecoration(context, LinearLayoutManager(context).orientation)
+
+    if (enabled) {
+        this.addItemDecoration(decoration)
+    } else {
+        this.removeItemDecoration(decoration)
+    }
 }
